3dfba12f4d2bfdc82359d8bf7e394bd34cfdee34
Martin Boström <martin.bostrom@gu.se>
2022-09-01 16:26:26 +0200

    Removed unnecessary lines from demo

--- demo/run_demo.sh
+++ demo/run_demo.sh
@@ -2,10 +2,9 @@
 
 # Paths
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-annotate_mut_effect_script="$SCRIPT_DIR"'/../data/annotate_mutation_effects_in_gene_cds.R'
 demo_config="$SCRIPT_DIR"'/demo_config.yaml'
 
 # Run SEISMIC on demo data (TCGA UCEC)
 echo 'Running SEISMIC on the demo data.'
 cd "$SCRIPT_DIR"'/..'
-Rscript SEISMIC.R "$demo_config"
+./SEISMIC.R "$demo_config"
